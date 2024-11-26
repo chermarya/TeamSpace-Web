6c248fdd7d020b6ae2e25db69167fa3118078c72
Богдан Рикуш <bohdan.rykush@nure.ua>
2024-11-26 15:34:07 +0100

    Port test

--- NoteServer.js
+++ NoteServer.js
@@ -1,14 +1,5 @@
-const { createServer } = require('node:http');
+const PORT = process.env.PORT || 3000;
 
-const hostname = '127.0.0.1';
-const port = 3000;
-
-const server = createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+app.listen(PORT, () => {
+    console.log(`Сервер працює на порті ${PORT}`);
 });
